17fe2918dc6a543d3e631a976b2840f3de3e0301
nic-instruction <nicole.bade@seattlecolleges.edu>
2017-05-21 20:05:46 -0700

    Update generate_config.sh

--- nagios/generate_config.sh
+++ nagios/generate_config.sh
@@ -11,9 +11,31 @@ host="$1"
 ip="$2"
 
 echo "
+# Host Definition
+define host {
+    use         linux-server        ; Inherit default values from a template
+    host_name   $host               ; The name we're giving to this host
+    alias       web server          ; A longer name associated with the host
+    address     $ip                 ; IP address of the host
+}
+# Service Definition
+define service{
+        use                             generic-service         ; Name of service template to
+ use
+        host_name                       $host
+        service_description             load
+        check_command                   check_nrpe!check_load
+}
+define service{
+        use                             generic-service         ; Name of service template to
+ use
+        host_name                       $host
+        service_description             users
+        check_command                   check_nrpe!check_users
+}
 
 
-">> /ect/nagios/conf.d/"$hostname".cfg
+">> /ect/nagios/conf.d/"$host".cfg
 
 
 
