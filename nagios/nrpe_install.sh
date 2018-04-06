3041d0326a3b10219d7c72f720d58d2b4766120e
nic-instruction <nicole.bade@seattlecolleges.edu>
2018-04-05 20:06:18 -0700

    Update nrpe_install.sh

--- nagios/nrpe_install.sh
+++ nagios/nrpe_install.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 apt-get -y install nagios-nrpe-server nagios-plugins
 sed -i 's/allowed_hosts=127.0.0.1/allowed_hosts=127.0.0.1, 10.142.0.15/g' /etc/nagios/nrpe.cfg
+systemctl restart nagios-nrpe-server
