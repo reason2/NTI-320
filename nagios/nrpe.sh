4f0961d0c7e82181b0afa6ecb207bbdb7d5c453c
nic-instruction <nicole.bade@seattlecolleges.edu>
2018-04-12 17:30:15 -0700

    Update nrpe.sh

--- nagios/nrpe.sh
+++ nagios/nrpe.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+###############
+#On the server#
+###############
 # Need to put the NRPE changes into your config file
 echo "########### NRPE CONFIG LINE #######################
         define command{
@@ -7,8 +10,12 @@ echo "########### NRPE CONFIG LINE #######################
 }" >> /etc/nagios/objects/commands.cfg
 
 
+###############
+#On the client#
+###############
 sed -i "s,command[check_hda1]=/usr/lib64/nagios/plugins/check_disk -w 20% -c 10% -p /dev/hda1,command[check_disk]=/usr/lib64/nagios/plugins/check_disk -w 20% -c 10% -p /dev/sda1,g" /etc/nagios/nrpe.cfg
 
 echo "command[check_mem]=/usr/lib64/nagios/plugins/check_mem.sh -w 80 -c 90" >> /etc/nagios/nrpe.cfg
 
 # this should happen as part of the packaging of the rpm...
+/etc/init.d/nagios-nrpe-server restart
