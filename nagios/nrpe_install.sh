9b7c020fb7e9babe9e311d216e6cf99b9b464366
nic-instruction <nicole.bade@seattlecolleges.edu>
2019-04-04 19:24:33 -0700

    Update nrpe_install.sh

--- nagios/nrpe_install.sh
+++ nagios/nrpe_install.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 yum install -y nagios-nrpe-server nagios-plugins nrpe
+systemctl enable nrpe
+systemctl start nrpe
 sed -i 's/allowed_hosts=127.0.0.1/allowed_hosts=127.0.0.1, 10.142.0.15/g' /etc/nagios/nrpe.cfg
 systemctl restart nrpe
