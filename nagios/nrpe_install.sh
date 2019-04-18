614e47d09175f14118cab3409db52796ba56ead1
nic-instruction <nicole.bade@seattlecolleges.edu>
2019-04-18 16:03:49 -0700

    Update nrpe_install.sh

--- nagios/nrpe_install.sh
+++ nagios/nrpe_install.sh
@@ -3,4 +3,5 @@ yum install -y nagios-nrpe-server nagios-plugins nrpe
 systemctl enable nrpe
 systemctl start nrpe
 sed -i 's/allowed_hosts=127.0.0.1/allowed_hosts=127.0.0.1, 10.142.0.15/g' /etc/nagios/nrpe.cfg
+sed -i "s,command[check_hda1]=/usr/lib64/nagios/plugins/check_disk -w 20% -c 10% -p /dev/hda1,command[check_disk]=/usr/lib64/nagios/plugins/check_disk -w 20% -c 10% -p /dev/sda1,g" /etc/nagios/nrpe.cfg
 systemctl restart nrpe
