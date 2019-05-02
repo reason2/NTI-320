c3975b6af6f7ff8f65c02dfc62b227f80ec6929c
nic-instruction <nicole.bade@seattlecolleges.edu>
2019-05-02 13:43:36 -0700

    Update for_loop.sh

--- automation/for_loop.sh
+++ automation/for_loop.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 for servername in $(gcloud compute instances list | awk '{print $1}' | sed "1 d" );  do 
     echo $servername;
-    serverip=$( gcloud compute instances list | grep $servername | awk '{print $5}');
+    serverip=$( gcloud compute instances list | grep $servername | awk '{print $4}');
     echo $serverip ;
 done
