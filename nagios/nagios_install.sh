5fb050fe0c01f3a3c2f8c08f4eac6f993674c8c0
nic-instruction <nicole.bade@seattlecolleges.edu>
2020-04-16 11:08:09 -0700

    Update nagios_install.sh

--- nagios/nagios_install.sh
+++ nagios/nagios_install.sh
@@ -32,86 +32,16 @@ echo 'define command{
 
 systemctl restart nagios
 
-cd /etc/nagios/servers
+yum -y install wget
+cd /etc/nagios
+wget https://raw.githubusercontent.com/nic-instruction/hello-nti-320/master/generate_config.sh
+
+
 
 # Now take a break, and spin up a machine called example-a with all the nrpe plugins installed and a propperly configured path
 # to nagios
-# put it's ip address into the address column below
+# put it's ip address and hostname insto generate_config.sh
 
-echo '# Define a host for an example-a machine
-define host{
-        use                     linux-server
-        host_name               example-a
-        alias                   example-a
-        address                 10.138.0.2
-        }
-###############################################################################
-###############################################################################
-#
-# SERVICE DEFINITIONS
-#
-###############################################################################
-###############################################################################
-# Define a service to ping the example-a machine
-define service{
-        use                             generic-service         ; Name of service template to use
-        host_name                       example-a
-        service_description             PING
-        check_command                   check_ping!100.0,20%!500.0,60%
-        }
-# Define a service to check HTTP on the example-a machine.
-# Disable notifications for this service by default, as not all users may have HTTP enabled.
-define service{
-        use                             generic-service         ; Name of service template to use
-        host_name                       example-a
-        service_description             HTTP
-        check_command                   check_http
-        notifications_enabled           0
-        }
-# Define a service to check the disk space of the root partition
-# on the example-a machine.  Warning if < 20% free, critical if
-# < 10% free space on partition.
-define service{
-        use                             generic-service         ; Name of service template to use
-        host_name                       example-a
-        service_description             Root Partition
-        check_command			              check_nrpe!check_disk!20%!10%!/
-        }
-# Define a service to check the number of currently logged in
-# users on the example-a machine.  Warning if > 20 users, critical
-# if > 50 users.
-define service{
-        use                             generic-service         ; Name of service template to use
-        host_name                       example-a
-        service_description             Current Users
-        check_command			              check_nrpe!check_users!20!50
-        }
-# Define a service to check the number of currently running processes
-# on the example-a machine.  Warning if > 250 processes, critical if
-# > 400 processes.
-define service{
-        use                             generic-service         ; Name of service template to use
-        host_name                       example-a
-        service_description             Total Processes
-        check_command			              check_nrpe!check_procs!250!400!RSZDT
-        }
-# Define a service to check the load on the example-a machine.
-define service{
-        use                             generic-service         ; Name of service template to use
-        host_name                       example-a
-        service_description             Current Load
-        check_command			              check_nrpe!check_load!5.0,4.0,3.0!10.0,6.0,4.0
-        }
-# Define a service to check SSH on the example-a machine.
-# Disable notifications for this service by default, as not all users may have SSH enabled.
-define service{
-        use                             generic-service         ; Name of service template to use
-        host_name                       example-a
-        service_description             SSH
-        check_command			              check_ssh
-        notifications_enabled		        0
-        }
-' > example-a.cfg
 
 Further configuration:
 https://assets.nagios.com/downloads/nagioscore/docs/nagioscore/4/en/monitoring-publicservices.html (Links to an external site.)
