edbd0d532616ad8e28d58d1d86a8eac289b663b6
nic-instruction <nicole.bade@seattlecolleges.edu>
2020-04-16 12:43:25 -0700

    Update nrpe_install.sh

--- nagios/nrpe_install.sh
+++ nagios/nrpe_install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-yum install -y nagios-plugins nrpe nagios-plugins-load nagios-plugins-ping nagios-plugins-disk nagios-plugins-http nagios-plugins-procs wget
+yum install -y nagios-plugins nrpe nagios-plugins-load nagios-plugins-ping nagios-plugins-disk nagios-plugins-http nagios-plugins-procs wget yum -y install nagios-plugins-all
 # BUG:https://osric.com/chris/accidental-developer/2016/12/missing-nagios-plugins-in-centos-7/ (nrpe plugins have been packaged seperately and don't install with nagios-plugins-all)
 # BUG #2 https://cloudwafer.com/blog/installing-nagios-agent-npre-on-centos/ (the nrpe config is commented out and checks are not defined)
 # Use sed statments to uncomment NRPE config and add the appropiate flags
