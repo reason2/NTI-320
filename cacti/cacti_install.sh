ced4e7ef1ea2b2922703eb56206adfcdb04575ed
nic-instruction <nicole.bade@seattlecolleges.edu>
2020-04-23 13:46:30 -0700

    Update cacti_install.sh

--- cacti/cacti_install.sh
+++ cacti/cacti_install.sh
@@ -15,7 +15,6 @@ yum clean all
 yum -y install MariaDB-server MariaDB-client 
 
 yum -y install cacti               # Installes a number of packages, including mariadb, httpd, php and so on
-yum -y install mariadb-server         # The mysql/mariadb client installs with the cacti stack but not the server
                                    # If you want to have multiple cacti nodes, considder using the client and connecting
                                    # to another server
                                    
